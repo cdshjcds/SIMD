8cc295d2ebe2f462306cfdaa1ba7bcb831db4f35
cdshjcds <4527715239@163.com>
2023-04-22 16:45:16 +0800

    update

--- Project1/Project1/1.cpp
+++ Project1/Project1/1.cpp
@@ -106,59 +106,6 @@ void avx2(int m, int n, float a[][max_N]) {
 }
 
 void avx3(int m, int n, float a[][max_N]) {
-	__m256 t1, t2, t3, c;
-	int T = 32;
-	for (int k = 0; k < (m < n ? m : n); k++)
-	{
-		//选取k列绝对值最大的元素为主元,主元在row行
-		float s = abs(a[k][k]);
-		int row = k;
-		for (int i = k + 1; i < m; i++)
-		{
-			float t = abs(a[i][k]);
-			if (s < t)
-			{
-				s = t;
-				row = i;
-			}
-		}
-		//k列全为0则不需处理
-		if (s == 0)
-			continue;
-		//交换row行与k行
-		if (row != k)
-			for (int j = 0; j < n; j++)
-				swap(a[k][j], a[row][j]);
-		for (int j = k + 1; j < n; j++)
-			a[k][j] = a[k][j] / a[k][k];
-		a[k][k] = 1;
-		for (int j = k + 1; j % 4 != 0; j++)
-			for (int i = k + 1; i < m; i++)
-				a[i][j] = a[i][j] - a[i][k] * a[k][j];
-		for (int t = 0; t < ((m - k - 1) % T > 0 ? 1 + (m - k - 1) / T : (m - k - 1) / T); t++)
-		{
-			for (int j = k + 1 + (4 - (k + 1) % 4) % 4; j <= n - 8; j += 8)
-			{
-				t1 = _mm256_loadu_ps(a[k] + j);
-				for (int i = k + 1 + t * T; i < (m > k + 1 + t * T + T ? k + 1 + t * T + T : m); i++)
-				{
-					t2 = _mm256_loadu_ps(a[i] + j);
-					c = _mm256_set1_ps(a[i][k]);
-					t3 = _mm256_mul_ps(t1, c);
-					t2 = _mm256_sub_ps(t2, t3);
-					_mm256_store_ps(a[i] + j, t2);
-				}
-			}
-		}
-		for (int j =  n - (n - k - 1 - (4 - (k + 1) % 4) % 4) % 8 ; j < n; j++)
-			for (int i = k + 1; i < m; i++)
-				a[i][j] = a[i][j] - a[i][k] * a[k][j];
-		for (int i = k + 1; i < m; i++)
-			a[i][k] = 0;
-	}
-}
-
-void avx4(int m, int n, float a[][max_N]) {
 	__m256 t, c;
 	for (int k = 0; k < (m < n ? m : n); k++)
 	{
